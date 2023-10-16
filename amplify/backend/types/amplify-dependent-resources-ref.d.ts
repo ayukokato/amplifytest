3b6f46d2edc297ebe39312c3781d7c6ccce33205
ayuko-kato <ayuko.kato@st-hakky.com>
2023-10-16 20:49:06 +0900

    done

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reacttutorial": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
